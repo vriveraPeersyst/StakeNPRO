0e408ef28f24476c9a3192a5cfbfb37a19645de7
vrc <vrivera@peersyst.com>
2025-09-12 13:17:07 +0200

    suppressHydrationWarning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </head>
-      <body>
+      <body suppressHydrationWarning={true}>
         <Providers>
           {children}
         </Providers>
